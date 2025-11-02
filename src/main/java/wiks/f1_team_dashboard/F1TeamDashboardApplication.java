ce01fccb144b8d7d3497e1765305de609ad0f804
wiktorry <wiktorszulc4@gmail.com>
2025-11-02 19:14:44 +0100

    feat: initial commit

---
+++ src/main/java/wiks/f1_team_dashboard/F1TeamDashboardApplication.java
@@ -0,0 +1,13 @@
+package wiks.f1_team_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1TeamDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1TeamDashboardApplication.class, args);
+	}
+
+}
